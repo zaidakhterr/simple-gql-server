79290ce72d0044d27f8a53bfa7b80a4647a4cbd4
Zaid Akhter <zaidakhter1202@gmail.com>
2020-08-27 20:22:24 +0500

    📦 NEW: book query

--- index.js
+++ index.js
@@ -2,30 +2,37 @@ const { ApolloServer, gql } = require("apollo-server");
 
 const books = [
   {
+    id: "1",
     title: "Harry Potter and the Philosopher's Stone",
     author: "J.K. Rowling",
   },
   {
+    id: "2",
     title: "Harry Potter and the Chamber of Secrets",
     author: "J.K. Rowling",
   },
   {
+    id: "3",
     title: "Harry Potter and the Prisoner of Azkaban",
     author: "J.K. Rowling",
   },
   {
+    id: "4",
     title: "Harry Potter and the Goblet of Fire",
     author: "J.K. Rowling",
   },
   {
+    id: "5",
     title: "Harry Potter and the Order of the Phoenix",
     author: "J.K. Rowling",
   },
   {
+    id: "6",
     title: "Harry Potter and the Half-Blood Prince",
     author: "J.K. Rowling",
   },
   {
+    id: "7",
     title: "Harry Potter and the Deathly Hallows",
     author: "J.K. Rowling",
   },
@@ -33,18 +40,21 @@ const books = [
 
 const typeDefs = gql`
   type Book {
-    title: String
-    author: String
+    id: ID!
+    title: String!
+    author: String!
   }
 
   type Query {
     books: [Book]
+    book(id: ID!): Book
   }
 `;
 
 const resolvers = {
   Query: {
     books: () => books,
+    book: (_, { id }) => books.find((book) => book.id === id),
   },
 };
 
