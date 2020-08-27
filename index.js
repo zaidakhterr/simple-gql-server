c6c76bd6c0f7544d7237ce44bbdd552b07f448cd
Zaid Akhter <zaidakhter1202@gmail.com>
2020-08-27 21:58:27 +0500

    🐛 FIX: removeBook Mutation

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const typeDefs = gql`
   type Author {
     id: ID!
     name: String!
-    books: [ID!]
+    books: [ID]
   }
 
   type Book {
@@ -66,6 +66,7 @@ const resolvers = {
     books: () =>
       books.map((book) => {
         let author = authors.find((author) => author.books.includes(book.id));
+
         return {
           ...book,
           author,
@@ -73,7 +74,13 @@ const resolvers = {
       }),
     book: (_, { id }) => {
       let book = books.find((_book) => _book.id === id);
+
+      if (!book) {
+        throw new Error("Book does not Exist");
+      }
+
       let author = authors.find((author) => author.books.includes(book.id));
+
       return {
         ...book,
         author,
@@ -87,6 +94,7 @@ const resolvers = {
       if (!author) {
         throw new Error("Author does not Exist");
       }
+
       const newBook = {
         id: String(books.length),
         title,
@@ -99,15 +107,34 @@ const resolvers = {
       });
 
       books.push(newBook);
+
       return {
         ...newBook,
         author,
       };
     },
     removeBook: (_, { id }) => {
-      const deletedBook = books.find((book) => book.id === id);
-      books = books.filter((book) => book.id !== id);
-      return deletedBook;
+      const book = books.find((_book) => _book.id === id);
+
+      if (!book) {
+        throw new Error("Book does not Exist");
+      }
+
+      let author = authors.find((author) => author.books.includes(book.id));
+
+      authors.forEach((_author) => {
+        if (_author.id === author.id) {
+          let bookIndex = _author.books.findIndex((_book) => _book === book.id);
+          _author.books.splice(bookIndex, 1);
+        }
+      });
+
+      books = books.filter((_book) => _book.id !== book.id);
+
+      return {
+        ...book,
+        author,
+      };
     },
   },
 };
